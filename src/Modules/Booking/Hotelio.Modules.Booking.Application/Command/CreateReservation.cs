57983eeaa9890a0b64284009586a6464d4482945
Damian Kusek <damian.kusek@merce.com>
2023-09-06 22:05:23 +0200

    Create reservation validation

--- src/Modules/Booking/Hotelio.Modules.Booking.Application/Command/CreateReservation.cs
+++ src/Modules/Booking/Hotelio.Modules.Booking.Application/Command/CreateReservation.cs
@@ -4,14 +4,22 @@ using Hotelio.Shared.Commands;
 
 namespace Hotelio.Modules.Booking.Application.Command;
 
+using System.ComponentModel.DataAnnotations;
+
 public record CreateReservation(
-    string Id,
-    string HotelId,
+    [Required] string Id,
+    [Required] string HotelId,
+    [Range(1, int.MaxValue, ErrorMessage = "RoomType must be a positive integer.")]
     int RoomType,
+    [Range(1, int.MaxValue, ErrorMessage = "NumberOfGuests must be a positive integer.")]
     int NumberOfGuests,
+    [Range(0.01, double.MaxValue, ErrorMessage = "PriceToPay must be greater than 0.")]
     double PriceToPay,
+    [Range(1, int.MaxValue, ErrorMessage = "PaymentType must be a positive integer.")]
     int PaymentType,
+    [Range(typeof(DateTime), "2000-01-01", "9999-12-31", ErrorMessage = "Field StartDate is required")] 
     DateTime StartDate,
+    [Range(typeof(DateTime), "2000-01-01", "9999-12-31", ErrorMessage = "Field EndDate is required")] 
     DateTime EndDate,
     List<string> Amenities
 ): ICommand;
